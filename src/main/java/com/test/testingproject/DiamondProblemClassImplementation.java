086ad542564661a4465f40d2630496a4a0abfe48
Adarsh Shukla <sadarsh158@gmail.com>
2024-10-28 16:20:33 +0530

    minor updates

--- src/main/java/com/test/testingproject/DiamondProblemClassImplementation.java
+++ src/main/java/com/test/testingproject/DiamondProblemClassImplementation.java
@@ -10,5 +10,18 @@ public class DiamondProblemClassImplementation implements DiamondProblemInterfac
 	public static void main(String[] args) {
 		DiamondProblemClassImplementation diamondDemo = new DiamondProblemClassImplementation();
 		diamondDemo.m1();
+		
+//	 The diamond problem is a common issue in languages that support multiple inheritance, like C++. It arises when a class inherits from two or more classes that have a common base class. Here's a brief explanation of the diamond problem:
+//
+//Suppose class A is the base class.
+//Classes B and C both inherit from class A.
+//Class D then inherits from both B and C.
+//In this case, if class A has a method, and B and C do not override it, class D inherits the same method from both B and C. The ambiguity arises because it's unclear whether class D should inherit the method from B or C.
+//This problem is called the diamond problem because the inheritance structure looks like a diamond shape:
+//    A
+//   / \
+//  B   C
+//   \ /
+//    D
 	}
 }
